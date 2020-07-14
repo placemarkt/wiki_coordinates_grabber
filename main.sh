6d358c36ad333b7ab501c2ed65c952b7906883ec
rkoeze <rkoeze@gmail.com>
2020-07-14 10:39:27 -0700

    Condense code.

--- main.sh
+++ main.sh
@@ -1,44 +1,55 @@
 #!/usr/bin/bash
 
+# performance ideas
+#    - cut larger parts of multistream bz2 file
+#    - move idx loop inside index-formatting loop to remove reads/writes from disk
+#    - don't search in XML for id or title because you already have this information
+
 declare i
 declare j
+declare idarr
+declare titlearr
+
 while IFS=: read -r col1 col2 col3
   do
   if [ -z "$i" ]; then
     i=$col1
+    idarr=("$col2")
+    titlearr=("$col3")
   else
     if [ "$i" == "$col1" ]; then
+      idarr=("${idarr[@]}" "$col2")
+      titlearr=("${titlearr[@]}" "$col3")
+
       continue
     else
       j=$col1
-      echo "$i:$j" >> index-formatted.txt
+      idarr=("${idarr[@]}" "$col2")
+      titlearr=("${titlearr[@]}" "$col3")
+
+      rm -f temp.bz2
+      rm -f temp.xml       
+      ccount=`expr $j - $i`
+
+      dd if=enwiki-20200220-pages-articles-multistream.xml.bz2 iflag=skip_bytes,count_bytes skip=$i count=$ccount of=temp.bz2
+      ( echo "<content>" ; bunzip2 -c temp.bz2 ; echo "</content>" ) >> temp.xml
+
+      for idx in "${!idarr[@]}"; do
+        id="${idarr[$idx]}"
+        title="${titlearr[$idx]}"
+        coordinates=$(xmlstarlet sel -t -m "//page[id=$id]" -v "*" -n temp.xml | ack -i "\{\{coord.*display=(?!.*inline)" | xargs -I{} ./compiled {})
+        if [ -z "$coordinates" ]; then
+          continue
+        else
+          formatted_coordinates=$(echo $coordinates | GeoConvert)
+          echo "$id|$title|$formatted_coordinates" >> coords.csv
+        fi
+      done
+
       i=""
       j=""
+      idarr=("")
+      titlearr=("")
     fi
   fi
-done < enwiki-20200701-pages-articles-multistream-index.txt
-
-while IFS=: read -r si ei
-  do
-  rm -f temp.bz2
-  rm -f temp.xml
-  ccount=`expr $ei - $si`
-
-  dd if=enwiki-20200701-pages-articles-multistream.xml.bz2 iflag=skip_bytes,count_bytes skip=$si count=$ccount of=temp.bz2
-  ( echo "<content>" ; bunzip2 -c temp.bz2 ; echo "</content>" ) >> temp.xml
-
-  readarray -t ids < <(xmlstarlet sel -t -m "//page" -v "id" -n temp.xml)
-
-  for idx in "${!ids[@]}"; do
-    id="${ids[$idx]}"
-    title=$(xmlstarlet sel -t -m "//page[id=$id]" -v "title" -n temp.xml)
-    coordinates=$(xmlstarlet sel -t -m "//page[id=$id]" -v "*" -n temp.xml | ack -i "\{\{coord.*display=(?!.*inline)" | xargs -I{} ./compiled {})
-    if [ -z "$coordinates" ]; then
-      continue
-    else
-      formatted_coordinates=$(echo $coordinates | GeoConvert)
-      echo "$id|$title|$formatted_coordinates" >> coords.csv
-    fi
-  done
-  
-done < index-formatted.txt
+done < enwiki-20200220-pages-articles-multistream-index.txt
