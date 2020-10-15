c9bf167b6ed12c6076ddbfe777ba22656d5ffd7a
rkoeze <rkoeze@gmail.com>
2020-10-15 21:00:05 +0200

    Use gzipped file instead

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 # select(.labels.en != null) |
 
-bzip2 -d wikidata-20200817-all.json.bz2 \ 
-	| jq -r '[.labels.en.value, .claims.P625[0].mainsnak.datavalue.value.latitude, .claims.P625[0].mainsnak.datavalue.value.longitude]'
+# gunzip wikidata-20200817-all.json.bz2 \ 
+# 	| jq -r '[.labels.en.value, .claims.P625[0].mainsnak.datavalue.value.latitude, .claims.P625[0].mainsnak.datavalue.value.longitude]'
+
+
+zcat latest-all.json.gz | jq -cn --stream 'fromstream(1|truncate_stream(inputs))' \
+	| jq -c 'select((.labels.en != null) and (.claims.P625 != null)) | jq -cr [.labels.en.value, .claims.P625[0].mainsnak.datavalue.value.latitude, .claims.P625[0].mainsnak.datavalue.value.longitude]'
